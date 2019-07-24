3ce6ee94aff224aa7af6cd9d831cba7b9e3dd082
Sam <samogorm@gmail.com>
2019-07-24 20:03:30 +0100

    mobile version of projects section

--- src/components/project-block/ProjectBlock.js
+++ src/components/project-block/ProjectBlock.js
@@ -11,38 +11,40 @@ import './ProjectBlock.css'
 export const ProjectBlock = (props) => {
     return (
         <div className="project-block">
-            <div className="project-block-header">
-                <div className="project-icon">
-                    <img src={props.icon} alt={`${props.title} Icon`}/>
+           <div className="project-block-container">
+                <div className="project-block-header">
+                    <div className="project-icon">
+                        <img src={props.icon} alt={`${props.title} Icon`} />
+                    </div>
+                    <div className="project-tags">
+                        {props.tags.map((tag, index) => <Tag key={index} text={tag} />)}
+                    </div>
                 </div>
-                <div className="project-tags">
-                    {props.tags.map((tag, index) => <Tag key={index} text={tag} /> )}
+                <div className="project-block-title">
+                    <Subtitle text={props.title} />
                 </div>
-            </div>
-            <div className="project-block-title">
-                <Subtitle text={props.title} />
-            </div>
-            <div className="project-block-description">
-                <p> {props.description} </p>
-            </div>
-            <div className="project-block-footer">
-                <Button
-                    label="View source"
-                    icon={null}
-                    type="outline"
-                    primaryColour="transparent"
-                    secondaryColour=""
-                    link={props.sourceLink}
-                />
-                <Button
-                    label="Visit site"
-                    icon={null}
-                    type="fill"
-                    primaryColour=""
-                    secondaryColour="#fff"
-                    link={props.demoLink}
-                />
-            </div>
+                <div className="project-block-description">
+                    <p> {props.description} </p>
+                </div>
+                <div className="project-block-footer">
+                    <Button
+                        label="View source"
+                        icon={null}
+                        type="outline"
+                        primaryColour="transparent"
+                        secondaryColour=""
+                        link={props.sourceLink}
+                    />
+                    <Button
+                        label="Visit site"
+                        icon={null}
+                        type="fill"
+                        primaryColour=""
+                        secondaryColour="#fff"
+                        link={props.demoLink}
+                    />
+                </div>
+           </div>
         </div>
     )
 }

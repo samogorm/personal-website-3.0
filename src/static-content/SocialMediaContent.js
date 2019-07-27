b93dc6b3b6be6d5442cf734bab1fb76df932f4b1
Sam <samogorm@gmail.com>
2019-07-27 14:19:29 +0100

    Added social links

--- src/static-content/SocialMediaContent.js
+++ src/static-content/SocialMediaContent.js
@@ -9,7 +9,7 @@ export const SocialMediaContent = [
         id: 2,
         icon: 'fab fa-codepen',
         title: 'Codepen',
-        url: ''
+        url: 'https://codepen.io/SAM-O10'
     },
     {
         id: 3,
@@ -21,6 +21,6 @@ export const SocialMediaContent = [
         id: 4,
         icon: 'fab fa-linkedin',
         title: 'LinkedIn',
-        url: ''
+        url: 'https://www.linkedin.com/in/samantha-o-gorman-8b955284/'
     }
 ]

1f2d85f5d662079f706c6cef79bfcb45860269b1
Sam <samogorm@gmail.com>
2019-07-27 13:29:54 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import { themeColours } from './../constants/themeColours';
 import {Footer} from './../components/footer/Footer';

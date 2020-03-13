8e578d516bd0c1b34b5ecc570a0e3877397c100c
Sam <samjgorm@gmail.com>
2020-03-13 13:19:58 +0000

    Fix tabs in AboutContent.js

--- src/static-content/AboutContent.js
+++ src/static-content/AboutContent.js
@@ -1,15 +1,15 @@
 export const AboutContent = {
-        intro: {
-            title: "Hello! I'm Sam.",
-            subtitle: 'Full Stack Developer',
-            text: 'A Full Stack Developer with an interest in user interface design and an insatiable hunger for learning new development technologies.'
-        },
-        about: {
-            image: '',
-            textBlocks: [
-                'I enjoy coding, climbing, socialising and the occasional binge watch of my favourite TV shows.',
-                'I have a 2:1 in (BSc.) Computing and Website Development and I’m a Front End Developer at Sizzle Creative.',
-                'Based in Bolton, Manchester.'
-            ]
-        }
-    };
+  intro: {
+    title: "Hello! I'm Sam.",
+    subtitle: 'Full Stack Developer',
+    text: 'A Full Stack Developer with an interest in user interface design and an insatiable hunger for learning new development technologies.'
+  },
+  about: {
+    image: '',
+    textBlocks: [
+      'I enjoy coding, climbing, socialising and the occasional binge watch of my favourite TV shows.',
+      'I have a 2:1 in (BSc.) Computing and Website Development and I’m a Front End Developer at Sizzle Creative.',
+      'Based in Bolton, Manchester.'
+    ]
+  }
+};

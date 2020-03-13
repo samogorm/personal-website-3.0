bc4c566ca388def3ded57d0733f0de1dd99d2ccb
Sam <samjgorm@gmail.com>
2020-03-13 13:07:57 +0000

    Fix tabs on ProjectsContent.js

--- src/static-content/ProjectsContent.js
+++ src/static-content/ProjectsContent.js
@@ -1,77 +1,77 @@
 export const ProjectsContent = [
-    {
-        id: 1,
-        icon: require('./../assets/images/projects/currency-converter-icon.svg'),
-        title: 'Currency Converter',
-        description: 'A simple currency converter web app. I designed the app and developed the front end in React. I coded the back end API in NodeJS (TypeScript). The API gathers latest currency data every 3 days.',
-        links: {
-            demo: 'https://exchange-rates.netlify.com/',
-            source: 'https://github.com/samogorm/currency-converter'
-        },
-        tags: [
-            'React',
-            'TypeScript',
-            'Node',
-            'Express',
-            'MongoDB',
-            'UI Design'
-        ]
+  {
+    id: 1,
+    icon: require('./../assets/images/projects/currency-converter-icon.svg'),
+    title: 'Currency Converter',
+    description: 'A simple currency converter web app. I designed the app and developed the front end in React. I coded the back end API in NodeJS (TypeScript). The API gathers latest currency data every 3 days.',
+    links: {
+        demo: 'https://exchange-rates.netlify.com/',
+        source: 'https://github.com/samogorm/currency-converter'
     },
-    {
-        id: 2,
-        icon: require('./../assets/images/projects/calculator-icon.svg'),
-        title: 'Simple Calculator',
-        description: 'A simple calculator web app which was built so I could learn React Hooks. I designed the app and developed the front end in React.',
-        links: {
-            demo: 'https://samogorm.github.io/simple-calculator/',
-            source: 'https://github.com/samogorm/simple-calculator'
-        },
-        tags: [
-            'React',
-            'UI Design'
-        ]
+    tags: [
+        'React',
+        'TypeScript',
+        'Node',
+        'Express',
+        'MongoDB',
+        'UI Design'
+    ]
+  },
+  {
+    id: 2,
+    icon: require('./../assets/images/projects/calculator-icon.svg'),
+    title: 'Simple Calculator',
+    description: 'A simple calculator web app which was built so I could learn React Hooks. I designed the app and developed the front end in React.',
+    links: {
+        demo: 'https://samogorm.github.io/simple-calculator/',
+        source: 'https://github.com/samogorm/simple-calculator'
     },
-    {
-        id: 3,
-        icon: require('./../assets/images/projects/lifetime-icon.svg'),
-        title: 'Lifetime Project',
-        description: 'A small web application that shows you how much time you have left to live your life. It was very much inspired by Dhanish Gajjar\'s Lifeproject.',
-        links: {
-            demo: 'https://lifetimeproject.netlify.com/',
-            source: 'https://github.com/samogorm/lifetime'
-        },
-        tags: [
-            'React',
-            'UI Design'
-        ]
+    tags: [
+        'React',
+        'UI Design'
+    ]
+  },
+  {
+    id: 3,
+    icon: require('./../assets/images/projects/lifetime-icon.svg'),
+    title: 'Lifetime Project',
+    description: 'A small web application that shows you how much time you have left to live your life. It was very much inspired by Dhanish Gajjar\'s Lifeproject.',
+    links: {
+        demo: 'https://lifetimeproject.netlify.com/',
+        source: 'https://github.com/samogorm/lifetime'
     },
-    {
-        id: 4,
-        icon: require('./../assets/images/projects/simple-todos-icon.svg'),
-        title: 'Simple Todos',
-        description: 'A very simple web application that will add/remove todos using the browser\'s localStorage feature.',
-        links: {
-            demo: 'https://simple-todos.netlify.com/',
-            source: 'https://github.com/samogorm/simple-todos'
-        },
-        tags: [
-            'React',
-            'UI Design'
-        ]
+    tags: [
+        'React',
+        'UI Design'
+    ]
+  },
+  {
+    id: 4,
+    icon: require('./../assets/images/projects/simple-todos-icon.svg'),
+    title: 'Simple Todos',
+    description: 'A very simple web application that will add/remove todos using the browser\'s localStorage feature.',
+    links: {
+        demo: 'https://simple-todos.netlify.com/',
+        source: 'https://github.com/samogorm/simple-todos'
     },
-    {
-        id: 5,
-        icon: require('./../assets/images/projects/weather-app.svg'),
-        title: 'Basic Weather App',
-        description: 'A basic web application that will get your current location weather and allow you to add other weather locations.',
-        links: {
-            demo: 'https://basic-weather.netlify.com',
-            source: 'https://github.com/samogorm/weather-app'
-        },
-        tags: [
-            'React',
-            'Accuweather API',
-            'UI Design'
-        ]
-    }
-]
+    tags: [
+        'React',
+        'UI Design'
+    ]
+  },
+  {
+    id: 5,
+    icon: require('./../assets/images/projects/weather-app.svg'),
+    title: 'Basic Weather App',
+    description: 'A basic web application that will get your current location weather and allow you to add other weather locations.',
+    links: {
+        demo: 'https://basic-weather.netlify.com',
+        source: 'https://github.com/samogorm/weather-app'
+    },
+    tags: [
+        'React',
+        'Accuweather API',
+        'UI Design'
+    ]
+  }
+];
